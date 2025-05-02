9a6aada114c3a7e70c6789856f1a5ff1c18ef7b1
sinyoung0403 <tlstjs753@gmail.com>
2025-05-02 17:29:42 +0900

    [Feat] #2: AuthUser 수정

--- src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
+++ src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
@@ -35,7 +35,9 @@ class TodoControllerTest {
         // given
         long todoId = 1L;
         String title = "title";
-        AuthUser authUser = new AuthUser(1L, "email", UserRole.USER);
+        // User 의 Nickname 추가
+        String nickname = "nickname";
+        AuthUser authUser = new AuthUser(1L, "email", nickname, UserRole.USER);
         User user = User.fromAuthUser(authUser);
         UserResponse userResponse = new UserResponse(user.getId(), user.getEmail());
         TodoResponse response = new TodoResponse(
