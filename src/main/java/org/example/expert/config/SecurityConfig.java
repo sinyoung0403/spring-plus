01ebbee1a746cea9de2c0f173824ed581ca879ac
sinyoung0403 <tlstjs753@gmail.com>
2025-05-07 20:24:58 +0900

    [Feat] #9: Security Config 생성 - auth 경로 -> `permitAll` - admin 경로 -> `ADMIN` 만 가능

---
+++ src/main/java/org/example/expert/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package org.example.expert.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(c -> c.disable())
+                // Session 사용 안 할거임.
+                .sessionManagement(c
+                        -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // 경로 설정
+                .authorizeHttpRequests(
+                        c -> c
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
