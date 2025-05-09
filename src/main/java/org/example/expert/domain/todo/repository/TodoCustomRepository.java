da9f4178663bf7d3fabc02e5d79452c5690d87d1
sinyoung0403 <tlstjs753@gmail.com>
2025-05-09 14:31:05 +0900

    [Feat] #10: Query DSL 구현
    - 기존에 있는 Interface + Impl 구조를 변경 (다양한 구조 경험을 위해)
    - Query DSL 을 이용하여 Search 구현

--- src/main/java/org/example/expert/domain/todo/repository/TodoCustomRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoCustomRepository.java
@@ -1,9 +1,71 @@
 package org.example.expert.domain.todo.repository;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.todo.dto.response.QTodoSearchResponse;
+import org.example.expert.domain.todo.dto.response.TodoSearchResponse;
 import org.example.expert.domain.todo.entity.Todo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
-public interface TodoCustomRepository {
-    Optional<Todo> findByIdWithUser(Long todoId);
+import static org.example.expert.domain.todo.entity.QTodo.todo;
+import static org.springframework.util.StringUtils.hasText;
+
+@RequiredArgsConstructor
+@Repository
+public class TodoCustomRepository {
+    private final JPAQueryFactory queryFactory;
+
+    public Optional<Todo> findByIdWithUser(Long todoId) {
+        return Optional.ofNullable(
+                queryFactory
+                        .selectFrom(todo)
+                        .leftJoin(todo.user).fetchJoin()
+                        .where(todo.id.eq(todoId))
+                        .fetchOne()
+        );
+    }
+
+    public Page<TodoSearchResponse> searchTodos(String title, LocalDateTime startDateTime, LocalDateTime endDateTime, String nickname, Pageable pageable) {
+        JPAQuery<Long> countQuery = queryFactory
+                .select(todo.count())
+                .from(todo)
+                .where(titleContains(title),
+                        nicknameContains(nickname),
+                        todo.createdAt.between(startDateTime, endDateTime));
+
+        List<TodoSearchResponse> content = queryFactory
+                .select(new QTodoSearchResponse(
+                        todo.title,
+                        todo.managers.size(),
+                        todo.comments.size()
+                ))
+                .from(todo)
+                .where(titleContains(title),
+                        nicknameContains(nickname),
+                        todo.createdAt.between(startDateTime, endDateTime))
+                .orderBy(todo.createdAt.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        return PageableExecutionUtils.getPage(content, pageable, () -> countQuery.fetchOne());
+    }
+
+    private BooleanExpression titleContains(String title) {
+        return hasText(title) ? todo.title.contains(title) : null;
+    }
+
+    private BooleanExpression nicknameContains(String nickname) {
+        return hasText(nickname) ? todo.user.nickname.contains(nickname) : null;
+    }
 }
